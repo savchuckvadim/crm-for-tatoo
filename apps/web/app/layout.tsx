7da03c02b85de30038bb3580473efdae230d8950
Vadim Savchuk <savchuckvadim@gmail.com>
2025-12-05 10:41:50 +0300

    fxd next version

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
         <html suppressHydrationWarning>
             <head>
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+                    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
                     rel="stylesheet"
                 />
             </head>
--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,4 +1,12 @@
 import { nextJsConfig } from '@workspace/eslint-config/next-js';
 
-export default nextJsConfig;
+export default [
+    ...nextJsConfig,
+    {
+        rules: {
+            // Отключаем правило для App Router (в App Router шрифты загружаются через next/font/google в layout)
+            '@next/next/no-page-custom-font': 'off',
+        },
+    },
+];
 
